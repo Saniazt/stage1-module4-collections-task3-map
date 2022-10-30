269c54661d56c49ada6661b7dc2f25b7e2f95c81
saniazt <saniazt@gmail.com>
2022-10-30 17:06:44 +0200

    1

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,15 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> map = new HashMap<>();
+        for(Map.Entry<Integer, String> i : sourceMap.entrySet()){
+            map.put(i.getValue(), i.getKey());
+        }
+        map.remove("Friday",10);
+        return map;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,28 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < sentence.length(); i++) {
+            if(sentence.charAt(i) == '.')
+                sentence = sentence.replace(".","");
+            else if(sentence.charAt(i) == ',')
+                sentence = sentence.replace(",","");
+        }
+        String[] split = sentence.toLowerCase().split("\\s");
+        for (int i = 0; i < split.length; i++) {
+            int count = 0;
+            if(sentence.isEmpty())
+                break;
+            for (int j = 0; j < split.length; j++) {
+                if(split[i].equals(split[j]))
+                    count++;
+            }
+            map.put(split[i], count);
+        }
+        return map;
     }
 }
